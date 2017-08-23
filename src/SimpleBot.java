fcd970d9390e1ace6224b07cd080426310fb6325
Андрей Лавринюк <splaandrey@gmail.com>
2017-08-23 20:55:57 +0200

    End

--- src/SimpleBot.java
+++ src/SimpleBot.java
@@ -26,17 +26,59 @@ public class SimpleBot {
 			"Давае сохраним интригу?"
 	};
 	
-	final Map<String, String> ANSWER_BY_PATTERNS = new HashMap<String, String>(){{
-		put("hello", "Здраствуйте, рад Вас видетью");
-		put("who", "Здраствуйте, рад Вас видетью");
-		put("name", "Здраствуйте, рад Вас видетью");
-		put("howareyou", "Здраствуйте, рад Вас видетью");
+	final Map<String, String> ANSWERS_BY_PATTERNS = new HashMap<String, String>(){{
+		put("hello", "Здраствуйте, рад Вас видеть.");
+		put("who", "Здраствуйте, рад Вас видеть.");
+		put("name", "Здраствуйте, рад Вас видеть.");
+		put("howareyou", "Здраствуйте, рад Вас видеть.");
 		put("whatdoyoudoing", "Я пробую общатся с людьми.");
 		put("whatdoyoulike", "Мне нравится думать что я непросто программа");
 		put("iamfeelling", "Как давно это началось? ");
 		put("yes", "Согласие есть продукт при полном непротивлении сторон");
 		put("bye", "До сидания. Надеюсь ещё увидемся.");
 	}};
+	
+	final Map<String, String> PATTERNS_FOR_ANALYSTIS = new HashMap<String, String>(){{
+		//hello
+		put("хай", "hello");
+		put("привет", "hello");
+		put("здорово", "hello");
+		put("здравствуй", "hello");
+		//who
+		put("кто\\s.*ты", "who");
+		put("ты\\s.*кто", "who");
+		//name
+		put("как\\s.*зовут", "name");
+		put("как\\s.*имя", "name");
+		put("есть\\s.*имя", "name");
+		put("какое\\s.*имя", "name");
+		//howareyou
+		put("как\\s.*дела", "howareyou");
+		put("как\\s.*жизнь", "howareyou");
+		//whatdoyoudoing
+		put("зачем\\s.*тут", "whatdoyoudoing");
+		put("зачем\\s.*здесь", "whatdoyoudoing");
+		put("что\\s.*делаешь", "whatdoyoudoing");
+		put("чем\\s.*занимаешся", "whatdoyoudoing");
+		//whatdoyoulike
+		put("что\\s.*нравится", "whatdoyoulike");
+		put("что\\s.*любишь", "whatdoyoulike");
+		//iamfeelling
+		put("кажется", "iamfeelling");
+		put("чувствую", "iamfeelling");
+		put("испытываю", "iamfeelling");
+		//yes
+		put("да", "yes");
+		put("согласен", "yes");
+		//whattime
+		put("который\\s.*час", "whattime");
+		put("сколько\\s.*время", "whattime");
+		//bye
+		put("прощай", "bye");
+		put("увидимся", "bye");
+		put("до\\s.*свидания", "bye");
+	}};
+	
 	Pattern pattern;    //for regexp
 	Random random;      //for random answer
 	Date date;          //for date and time
@@ -56,17 +98,17 @@ public class SimpleBot {
 		ELUSIVE_ANSWERS[random.nextInt(ELUSIVE_ANSWERS.length)] :
 				COMMON_PHRASES[random.nextInt(COMMON_PHRASES.length)];
 		
-	/*	if (ai) {
+		if (ai) {
 			String message =
 					String.join(" ", msg.toLowerCase().split("[ {,|.}?]+"));
-			for (Map.Entry<String, String> o : PATTERNS_FOR_ANALYSIS.entrySet()) {
+			for (Map.Entry<String, String> o : PATTERNS_FOR_ANALYSTIS.entrySet()) {
 				pattern = Pattern.compile(o.getKey());
 				if (pattern.matcher(message).find()) {
 					if (o.getValue().equals("whattime")) return date.toString();
 					else return ANSWERS_BY_PATTERNS.get(o.getValue());
 				}
 			}
-		}*/
+		}
 		return say;
 	}
 }
